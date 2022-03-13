e55a87a5c1a7213278625151263b7632a72405a5
Prajwal Adhikari <prajwalad101@gmail.com>
2022-03-13 16:57:00 +0545

    protect all client routes using jwt sessions

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+
+import Auth from "../components/wrappers/Auth";
+import Provider from "../components/wrappers/Provider";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider session={pageProps.session}>
+      <Auth>
+        <Component {...pageProps} />
+      </Auth>
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,15 @@
+import { useSession } from "next-auth/react";
+
 export default function Home() {
-  return <div></div>;
+  const { data: session, status } = useSession();
+
+  const { name, email, image } = session?.user;
+
+  return (
+    <div>
+      <h1>User</h1>
+      <p>Name: {name}</p>
+      <p>Email: {email}</p>
+    </div>
+  );
 }
