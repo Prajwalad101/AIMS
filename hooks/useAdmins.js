30b71271d138b500ec49a3c7b41e27922da1c2c4
Prajwal Adhikari <prajwalad101@gmail.com>
2022-03-16 21:45:56 +0545

    create hook to fetch admin data

---
+++ hooks/useAdmins.js
@@ -0,0 +1,32 @@
+import { useMutation, useQueryClient } from "react-query";
+
+function useAdmins() {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation(
+    async (newAdmin) => {
+      const res = await fetch(`/api/admin`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify,
+      });
+
+      if (!res.ok) {
+        throw Error("Error while adding admin to the database");
+      }
+
+      return res;
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(["admins"]);
+      },
+    }
+  );
+
+  return mutation;
+}
+
+export default useAdmins;
