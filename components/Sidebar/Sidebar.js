0185c56319af654ca07ad07da039c33ef740a725
Prajwal Adhikari <prajwalad101@gmail.com>
2022-03-24 12:53:06 +0545

    add page navigation for individual sidebar link

--- components/Sidebar/Sidebar.js
+++ components/Sidebar/Sidebar.js
@@ -1,13 +1,17 @@
 import { useRouter } from "next/router";
 import { useState } from "react";
 
+import { farmerSidebarData, adminSidebarData } from "./Sidebar-data";
+
 function Sidebar({ user }) {
   const router = useRouter();
   const [selectedOption, setSelectedOption] = useState("dashboard");
 
-  const userHandler = () => {
-    console.log("Link clicked");
-    user === "farmer" ? router.push("/personal-info") : router.push("/users");
+  // change the sidebar data depending the logged in user
+  const sidebarData = user === "farmer" ? farmerSidebarData : adminSidebarData;
+
+  const linkHandler = (item) => {
+    router.push(`${item.url}`);
   };
 
   return (
@@ -19,42 +23,19 @@ function Sidebar({ user }) {
           </span>
         </a>
         <ul className="space-y-2">
-          <li>
-            <a
-              className={`flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 ${
-                selectedOption === "dashboard" ? "bg-gray-900" : ""
-              }`}
+          {sidebarData.map((item) => (
+            <li
+              key={item.id}
+              className="hover:cursor-pointer"
+              onClick={() => linkHandler(item)}
             >
-              <span className="ml-3">Dashboard</span>
-            </a>
-          </li>
-          <li onClick={userHandler}>
-            <a className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-              <span className="flex-1 ml-3 whitespace-nowrap">
-                {user === "admin" && "Users"}
-                {user === "farmer" && "Personal Info"}
-              </span>
-            </a>
-          </li>
-          <li>
-            <a className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-              <span className="flex-1 ml-3 whitespace-nowrap">Inbox</span>
-              <span className="inline-flex justify-center items-center p-3 ml-3 w-3 h-3 text-sm font-medium text-blue-600 bg-blue-200 rounded-full dark:bg-blue-900 dark:text-blue-200">
-                3
-              </span>
-            </a>
-          </li>
-
-          <li>
-            <a className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-              <span className="flex-1 ml-3 whitespace-nowrap">Products</span>
-            </a>
-          </li>
-          <li>
-            <a className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-              <span className="flex-1 ml-3 whitespace-nowrap">Log out</span>
-            </a>
-          </li>
+              <a
+                className={`flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 `}
+              >
+                <span className="ml-3">{item.title}</span>
+              </a>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
