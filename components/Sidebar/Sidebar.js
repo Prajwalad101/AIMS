652adc6f60c473059cec68428cd53103ca398158
Prajwal Adhikari <prajwalad101@gmail.com>
2022-03-24 13:02:49 +0545

    create pages for all the sidebar links

--- components/Sidebar/Sidebar.js
+++ components/Sidebar/Sidebar.js
@@ -5,12 +5,13 @@ import { farmerSidebarData, adminSidebarData } from "./Sidebar-data";
 
 function Sidebar({ user }) {
   const router = useRouter();
-  const [selectedOption, setSelectedOption] = useState("dashboard");
+  const [selectedOption, setSelectedOption] = useState("Dashboard");
 
   // change the sidebar data depending the logged in user
   const sidebarData = user === "farmer" ? farmerSidebarData : adminSidebarData;
 
   const linkHandler = (item) => {
+    setSelectedOption(item.title);
     router.push(`${item.url}`);
   };
 
@@ -30,7 +31,9 @@ function Sidebar({ user }) {
               onClick={() => linkHandler(item)}
             >
               <a
-                className={`flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 `}
+                className={`flex items-center p-2 text-base font-normal text-white rounded-lg hover:bg-gray-700 ${
+                  selectedOption === item.title ? "bg-gray-700" : ""
+                }`}
               >
                 <span className="ml-3">{item.title}</span>
               </a>
--- components/wrappers/Layout.js
+++ components/wrappers/Layout.js
@@ -2,7 +2,7 @@ import Sidebar from "../Sidebar/Sidebar";
 
 function Layout({ children, user }) {
   return (
-    <div>
+    <div className="flex">
       <Sidebar user={user.current} />
       {children}
     </div>
