5e303e6ab6703cb6212f989e2e3de3fa5439bfed
Prajwal Adhikari <prajwalad101@gmail.com>
2022-03-24 12:52:23 +0545

    add sidebar data for refactoring

---
+++ components/Sidebar/Sidebar-data.js
@@ -0,0 +1,55 @@
+export const farmerSidebarData = [
+  {
+    title: "Dashboard",
+    id: "1",
+    url: "/dashboard",
+  },
+  {
+    title: "Personal Info",
+    id: "2",
+    url: "/personal-info",
+  },
+  {
+    title: "Inbox",
+    id: "3",
+    url: "/inbox",
+  },
+  {
+    title: "Products",
+    id: "4",
+    url: "/products",
+  },
+  {
+    title: "Log out",
+    id: "5",
+    url: "/logout",
+  },
+];
+
+export const adminSidebarData = [
+  {
+    title: "Dashboard",
+    id: "1",
+    url: "/dashboard",
+  },
+  {
+    title: "Users",
+    id: "2",
+    url: "/users",
+  },
+  {
+    title: "Inbox",
+    id: "3",
+    url: "/inbox",
+  },
+  {
+    title: "Products",
+    id: "4",
+    url: "/products",
+  },
+  {
+    title: "Log out",
+    id: "5",
+    url: "/logout",
+  },
+];
