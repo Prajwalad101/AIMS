ba7f6ca36bd85103fed64a2c5aaf48883998338a
Prajwal Adhikari <prajwalad101@gmail.com>
2022-03-24 21:31:25 +0545

    add hook to validate user

---
+++ hooks/users/useValidateUser.js
@@ -0,0 +1,30 @@
+import { useMutation, useQueryClient } from "react-query";
+
+function useValidateUser() {
+  const queryClient = useQueryClient();
+
+  // create a mutation
+  const mutation = useMutation(
+    async (userDetails) => {
+      const res = await fetch("/api/verify-user", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userDetails),
+      });
+
+      if (!res.ok) {
+        throw Error("Error when adding user info for validation");
+      }
+      return res;
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(["users"]);
+      },
+    }
+  );
+}
+
+export default useValidateUser;
