0e4d3e199cc4902b10b191214e569813763a6b53
Prajwal Adhikari <prajwalad101@gmail.com>
2022-03-25 12:02:56 +0545

    add handler function to return logged in users info

---
+++ pages/api/users.js
@@ -0,0 +1,29 @@
+// utils
+import clientPromise from "../../lib/mongodb";
+import AppError from "../../lib/appError";
+
+// middlewares
+import catchAsync from "../../middlewares/catchAsync";
+import globalErrorHandler from "../../middlewares/errorMd";
+
+async function handler(req, res) {
+  const { method } = req;
+
+  if (method === "GET") {
+    const client = await clientPromise;
+    const usersCollection = client.db().collection("users");
+
+    const cursor = usersCollection.find();
+    const docs = await cursor.toArray();
+
+    return res.status(200).json({
+      status: "success",
+      message: docs,
+    });
+  } else {
+    const err = new AppError(`Could not find route for ${req.url}`, 404);
+    return globalErrorHandler(err, req, res);
+  }
+}
+
+export default catchAsync(handler);
