0abda81a8a87b9797bcedf64a2fdb5d55e5b099d
Prajwal Adhikari <prajwalad101@gmail.com>
2022-04-01 09:29:53 +0545

    create dashboard page for admin and user

---
+++ pages/admin/dashboard.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+import CropsChart from "../../components/Charts/CropsChart";
+import UsersChart from "../../components/Charts/UsersChart";
+import PriceChart from "../../components/Charts/PriceChart";
+import Tabs from "../../components/Tabs";
+import useUser from "../../hooks/users/useUser";
+import CropDetailChart from "../../components/Charts/CropDetailChart";
+
+function Dashboard({ userSession }) {
+  const id = userSession.current.id;
+  const { isLoading, isError, error, data } = useUser(id);
+
+  const [selectedChart, setSelectedChart] = useState("users");
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  const userData = data?.data;
+  const verificationStatus = userData.isVerified;
+
+  let userName = userData.name;
+  userName = userName.split(" ");
+  userName = userName[0];
+
+  return (
+    <div className="w-full mx-5 mt-5">
+      <div className="flex items-center gap-10">
+        <h1 className="font-ibm text-[25px] font-medium">
+          Welcome, {userName}
+        </h1>
+        <div className="flex gap-3 items-center font-poppins">
+          {/* <span className="bg-blue-100 text-blue-800 text-sm mr-2 px-3 py-1 rounded capitalize">
+            {verificationStatus}
+          </span> */}
+        </div>
+      </div>
+      <Tabs selectedChart={selectedChart} setSelectedChart={setSelectedChart} />
+      <div className="w-[95%]">
+        {selectedChart === "crops" && <CropsChart />}
+        {selectedChart === "users" && <UsersChart />}
+        {selectedChart === "price" && <PriceChart />}
+
+        {selectedChart === "crops-info" && (
+          <div className="flex">
+            <CropDetailChart />
+            <CropDetailChart />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
--- pages/user/dashboard.js
+++ pages/user/dashboard.js
@@ -1,16 +1,9 @@
 import { useState } from "react";
-
-import CropsChart from "../../components/Charts/CropsChart";
-import UsersChart from "../../components/Charts/UsersChart";
-import PriceChart from "../../components/Charts/PriceChart";
-import Tabs from "../../components/Tabs";
 import useUser from "../../hooks/users/useUser";
 
 function Dashboard({ userSession }) {
   const id = userSession.current.id;
   const { isLoading, isError, error, data } = useUser(id);
-
-  const [selectedChart, setSelectedChart] = useState("users");
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -22,28 +15,11 @@ function Dashboard({ userSession }) {
   let userName = userData.name;
   userName = userName.split(" ");
   userName = userName[0];
-
-  console.log(selectedChart);
-
   return (
-    <div className="w-full mx-5 mt-5">
-      <div className="flex items-center gap-10">
-        <h1 className="font-ibm text-[25px] font-medium">
-          Welcome, {userName}
-        </h1>
-        <div className="flex gap-3 items-center font-poppins">
-          {/* <p className="text-gray-500">Status: </p> */}
-          <span className="bg-blue-100 text-blue-800 text-sm mr-2 px-3 py-1 rounded capitalize">
-            {verificationStatus}
-          </span>
-        </div>
-      </div>
-      <Tabs selectedChart={selectedChart} setSelectedChart={setSelectedChart} />
-      <div className="w-[95%]">
-        {selectedChart === "crops" && <CropsChart />}
-        {selectedChart === "users" && <UsersChart />}
-        {selectedChart === "price" && <PriceChart />}
-      </div>
+    <div>
+      <span className="bg-blue-100 text-blue-800 text-sm mr-2 px-3 py-1 rounded capitalize">
+        {verificationStatus}
+      </span>
     </div>
   );
 }
