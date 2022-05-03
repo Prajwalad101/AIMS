983f163cd861c17990eacaad62451cd4bc7790c3
Prajwal Adhikari <prajwalad101@gmail.com>
2022-05-03 11:53:26 +0545

    fixed the layout of login screen on small screens

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -10,14 +10,14 @@ export default function SignIn({ providers }) {
   const router = useRouter();
 
   return (
-    <div className="mx-auto text-center mt-14 border-[1.5px] w-[45%] rounded-lg shadow-md">
+    <div className="mx-5 sm:mx-auto text-center mt-14 border-[1.5px] px-5 sm:w-[60%] rounded-lg shadow-md">
       <p className="font-ibm text-3xl font-medium mb-3 mt-10">
         Welcome to AIMS
       </p>
       <p className="font-poppins text-gray-500">
         Please select one of the login providers to log into your account
       </p>
-      <div className="pr-16">
+      <div className="sm:pr-16">
         <Image
           src={authImg}
           alt="login-image"
@@ -31,11 +31,11 @@ export default function SignIn({ providers }) {
           return (
             <div
               key={provider.name}
-              className="flex items-center justify-center w-[260px]"
+              className="flex items-center justify-center w-[250px] sm:w-[260px]"
             >
               <button
                 type="button"
-                className={`w-full flex gap-3 font-poppins items-center focus:ring-2 focus:outline-none focus:ring-blue-400 font-regular rounded-lg px-5 py-2.5 mr-2 mb-2 ${
+                className={`w-full flex gap-3 font-poppins items-center focus:ring-2 focus:outline-none focus:ring-blue-400 font-regular rounded-sm px-5 py-2.5 mb-2 ${
                   provider.id === "facebook"
                     ? "bg-[#4267B2] text-white hover:text-gray-100"
                     : "border-gray-400 border-[1.5px] hover:text-gray-500 "
