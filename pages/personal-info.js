76b8ddd6c64ec7ad8363cb2451d72428d5bc87de
Prajwal Adhikari <prajwalad101@gmail.com>
2022-05-09 21:34:54 +0545

    change input form validation

--- pages/personal-info.js
+++ pages/personal-info.js
@@ -197,7 +197,7 @@ export default function PersonalInfo() {
                       className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm text-sm border-gray-300 rounded-md"
                       required
                       minLength={3}
-                      maxLength={50}
+                      maxLength={90}
                     />
                   </div>
 
